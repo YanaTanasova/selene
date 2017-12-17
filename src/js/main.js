ed44defa18519f818e054b2fba0b286291a65511
YanaTanasova <domenika.12@bigmir.net>
2017-12-18 01:40:04 +0200

    second comit

--- src/js/main.js
+++ src/js/main.js
@@ -80,27 +80,36 @@ $(function() {
 		})
 	}
 
+	function changeControlClass(activeControl, reqControl){
+		activeControl.removeClass('current');
+		reqControl.addClass('current');
+	}
+
     (function () {
 		let counter = 1,
 			container = $('.home-slider__img-wrap'),
 			descContainer = $('.home-slider-item-desc__list'),
 			items = container.find('.home-slider__item'),
-			descItems = descContainer.find('.home-slider-item-desc__item');
+			descItems = descContainer.find('.home-slider-item-desc__item'),
+			controls = $('.home-slider__bottom-controls-item');
 
 		$('.home-slider__arrow--right').on('click', function(e){
 		    e.preventDefault();
 
 		    let activeItem = container.find('.home-slider__item.current'),
-		    	activeDesc = descContainer.find('.home-slider-item-desc__item.current');
+		    	activeDesc = descContainer.find('.home-slider-item-desc__item.current'),
+		    	activeControl = $('.home-slider__bottom-controls-item.current');
 
 			if (counter >= items.length) {
 				counter = 0;
 			}
 
 			let reqItem = items.eq(counter),
-				reqDescItem = descItems.eq(counter);
+				reqDescItem = descItems.eq(counter),
+				reqControl = controls.eq(counter);
 
 			nextSlide(activeItem, reqItem, activeDesc, reqDescItem);
+			changeControlClass(activeControl, reqControl);
 
 			counter++;
 
@@ -111,10 +120,12 @@ $(function() {
 		    e.preventDefault();
 
 		    let activeItem = container.find('.home-slider__item.current'),
-		    	activeDesc = descContainer.find('.home-slider-item-desc__item.current');
+		    	activeDesc = descContainer.find('.home-slider-item-desc__item.current'),
+		    	activeControl = $('.home-slider__bottom-controls-item.current');
 
 			let reqItem = items.eq(counter - 2),
-				reqDescItem = descItems.eq(counter - 2);
+				reqDescItem = descItems.eq(counter - 2),
+				reqControl = controls.eq(counter - 2);
 			reqItem.css('right', '100%');
 
 			activeItem.animate({
@@ -138,6 +149,8 @@ $(function() {
 				activeDesc.removeClass('current');
 				$(this).addClass('current');
 			})
+
+			changeControlClass(activeControl, reqControl);
 
 			counter--;
 
@@ -203,5 +216,125 @@ $(function() {
 
 		$('body').removeClass('overflow');
 	}
-	
+
+	// projects slider
+
+	(function () {
+		let counter = 0,
+			container = $('.projects__slider-left'),
+			items = container.find('.projects__slider-item'),
+			controls = $('.projects__slider-controls-item');
+
+		$('.project__slider-arrow--right').on('click', function(e){
+		    e.preventDefault();
+
+		    let activeItem = container.find('.projects__slider-item.current'),
+		    	activeControl = $('.projects__slider-controls-item.current');
+
+			if (counter >= items.length) {
+				counter = 0;
+			}
+
+			let reqItem = items.eq(counter),
+				reqControl = controls.eq(counter);
+
+			activeItem.animate({
+				'right' : '100%'
+			}, 500);
+
+			reqItem.animate({
+				'right' : '0'
+			}, 500, function () {
+				activeItem.removeClass('current').css('right', '-100%');
+				$(this).addClass('current');
+				changeControlClass(activeControl, reqControl);
+			});
+
+			counter++;
+		});
+
+		$('.project__slider-arrow--left').on('click', function(e){
+		    e.preventDefault();
+
+		    let activeItem = container.find('.projects__slider-item.current'),
+		    	activeControl = $('.projects__slider-controls-item.current');
+
+			let reqItem = items.eq(counter - 2),
+				reqControl = controls.eq(counter - 2);
+			reqItem.css('right', '100%');
+
+			activeItem.animate({
+				'right' : '-100%'
+			}, 500);
+
+			reqItem.animate({
+				'right' : '0'
+			}, 500, function () {
+				activeItem.removeClass('current');
+				$(this).addClass('current');
+				changeControlClass(activeControl, reqControl);
+			});
+
+			counter--;
+
+			if (counter <= 0) {
+				counter = items.length;
+			}
+		});
+
+		$('.projects__slider-controls-item').on('click', function(e){
+			e.preventDefault();
+			let controlId = $(this).index();
+
+			let activeItem = $('.projects__slider-item.current'),
+				reqItem = items.eq(controlId),
+				currentControl = $('.projects__slider-controls-item.current');
+
+			if (controlId + 1 != counter){
+
+				activeItem.animate({
+					'right' : '100%'
+				}, 500);
+
+				reqItem.animate({
+					'right' : '0'
+				}, 500, function () {
+					activeItem.removeClass('current').css('right', '-100%');
+					$(this).addClass('current');
+				});
+
+				currentControl.removeClass('current');
+				$(this).addClass('current');
+			}
+
+			counter = controlId + 1;
+		});
+	}());
+
+	// contact tabs
+
+	let tabsBtn = $('.contact__tabs'),
+        tabContent = $('.tab-content'),
+        childBtn = $('.contact-tabs__link'),
+        line = $('.line');
+
+    $(tabsBtn).on('click', '.contact-tabs__link', function(e){
+
+    	e.preventDefault();
+
+        let anchor = $(e.target).attr('href');
+
+        tabContent.animate({'opacity' : '0'}, 200);
+		childBtn.removeClass('current');
+		tabContent.removeClass('active-content');
+
+		$(e.target).addClass('current');
+		$(anchor).addClass('active-content').animate({'opacity' : '1'}, 200);
+        
+		let lineWidth = $('.contact-tabs__link.current').width();
+    	line.css('width', lineWidth);
+    	
+        let linePosition = $(e.target).position().left;
+        line.css('left', linePosition);
+    })
 })
